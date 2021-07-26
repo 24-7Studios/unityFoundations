f3becc664d2645f297380b6e3a4e9b09d8a2b7e7
HotshotCandy942 <HotshotCandy942@gmail.com>
2021-07-26 18:12:49 -0500

    fixed error with trying to spawn in broken guns, and started on movement

--- Assets/player scripts/old scipts/Player.cs
+++ Assets/player scripts/old scipts/Player.cs
@@ -5,85 +5,252 @@ using Mirror;
 
 public class Player : NetworkBehaviour
 {
-
+   
+    //player setup
+    
     public Rigidbody playerPhysBody;
     public Transform camTransformer;
     public GameObject CameraSetup;
     public Camera worldCam;
     public Camera gunCam;
     public GameObject weaponHolder;
+    public GameObject PlayerModelPrefab;
     public PlayerModelClass PlayerModel;
 
 
+    //mouse
     public float sens = 100f;
 
 
     public float yMouseInput = 0;
     public float xMouseInput = 0;
 
+    //movement
+    /*
+    public float moveSpeed = 2;
+    public float jumpForce = 5;
+    public float sprintMultiplyer = 2;
+    public float playerGravity = 9.86f;
+
+    public Transform groundCheck;
+    public Transform foot;
+    public Vector3 footPostition;
+    public LayerMask Jumpable;
+    public float groundDistance = 0.1f;
+    public float groundingForce = 0.05f;
+    public float maxAngle = 50;
+    public bool fly = false;
+    public bool usePhysicsGravity = false;
+
+    Vector3 InputMovement;
+    bool grounded;
+    bool canJump;
+    bool jump = false;
+    float x = 0;
+    float z = 0;
+    float y = 0;
+    Vector3 groundNormal;
+    */
 
     private void Start()
     {
 
-
+        
 
         if (!isLocalPlayer)
         {
-
+            
             foreach (GameObject part in PlayerModel.models)
             {
                 part.layer = 0;
             }
+            
+        }
 
+
+        if (isLocalPlayer)
+        {
+            
+            foreach (GameObject part in PlayerModel.models)
+            {
+                part.layer = 6;
+            }
             
 
-            return;
+
+            Instantiate(CameraSetup, camTransformer);
+
+
+
+            Cursor.lockState = CursorLockMode.Locked;
 
         }
 
-
-        foreach (GameObject part in PlayerModel.models)
-        {
-            part.layer = 6;
-        }
-
+        Instantiate(PlayerModel, playerPhysBody.transform);
         
+        /*
+        if (usePhysicsGravity)
+        {
+            playerGravity = Physics.gravity.y;
+        }
 
-        Instantiate(CameraSetup, camTransformer);
-
-
-
-        Cursor.lockState = CursorLockMode.Locked;
+        footPostition = foot.localPosition;
+        */
 
 
     }
 
-    //input
+    
     private void Update()
     {
 
 
+        //mouse looking
+        if (isLocalPlayer)
+        {
 
-        if (!isLocalPlayer) return;
-
-        float MouseX = Input.GetAxisRaw("Mouse X") * sens;
-        float MouseY = Input.GetAxisRaw("Mouse Y") * sens;
-
-
-        yMouseInput -= MouseY;
-        yMouseInput = Mathf.Clamp(yMouseInput, -90f, 90f);
-
-        xMouseInput -= MouseX;
-
-        camTransformer.transform.localRotation = Quaternion.Euler(Vector3.right * yMouseInput);
-        playerPhysBody.transform.rotation = Quaternion.Euler(Vector3.up * -xMouseInput);
-        CmdSyncPlayerRotation();
+            float MouseX = Input.GetAxisRaw("Mouse X") * sens;
+            float MouseY = Input.GetAxisRaw("Mouse Y") * sens;
 
 
+            yMouseInput -= MouseY;
+            yMouseInput = Mathf.Clamp(yMouseInput, -90f, 90f);
+
+            xMouseInput -= MouseX;
+
+            camTransformer.transform.localRotation = Quaternion.Euler(Vector3.right * yMouseInput);
+            playerPhysBody.transform.rotation = Quaternion.Euler(Vector3.up * -xMouseInput);
+            CmdSyncPlayerRotation();
+
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////
+
+
+        //movement
+        /*
+        if (isServer || isLocalPlayer)
+        {
+
+            grounded = isGrounded();
+            canJump = CanJump();
+
+
+
+            RaycastHit FloorSnap;
+
+            if (Physics.Raycast(groundCheck.position, -groundCheck.up, out FloorSnap) && ((FloorSnap.normal.x! < maxAngle) || (FloorSnap.normal.y! < maxAngle)) && grounded)
+            {
+
+                Quaternion toRotation = Quaternion.FromToRotation(transform.up, FloorSnap.normal) * transform.rotation;
+                groundCheck.rotation = toRotation;
+
+
+            }
+            else
+            {
+                groundCheck.rotation = playerPhysBody.rotation;
+
+            }
+
+            foot.rotation = groundCheck.rotation;
+            foot.localPosition = footPostition;
+
+        }
+
+        if (isLocalPlayer)
+        {
+
+            x = Input.GetAxisRaw("Horizontal");
+            z = Input.GetAxisRaw("Vertical");
+
+
+            if (fly)
+            {
+                if (Input.GetKey("space"))
+                {
+                    y = 1;
+                }
+                else if (Input.GetKey("c"))
+                {
+                    y = -1;
+                }
+                else
+                {
+                    y = 0;
+                }
+            }
+            else
+            {
+
+                if (Input.GetKeyDown("space") && canJump)
+                {
+                    jump = true;
+                }
+                if (grounded)
+                {
+                    y = -groundingForce;
+                }
+            }
+
+
+
+
+            if (fly)
+            {
+                InputMovement = (((camTransformer.transform.right * x + camTransformer.transform.forward * z) * moveSpeed) + camTransformer.transform.up * y);
+            }
+            else
+            {
+                InputMovement = ((((groundCheck.transform.right) * x + (groundCheck.transform.forward) * z) * moveSpeed) + groundCheck.transform.up * y);
+            }
+
+        }
+        */
+        ////////////////////////////////////////////////////////
 
 
 
     }
+
+
+    public void FixedUpdate()
+    {
+     /*
+        if(isServer)
+        {
+
+            if (!grounded)
+            {
+
+                y -= -playerGravity * Time.fixedDeltaTime;
+
+            }
+
+
+            playerPhysBody.AddForce(InputMovement, ForceMode.Impulse);
+
+            if (jump)
+            {
+                playerPhysBody.velocity += transform.up * jumpForce;
+                jump = false;
+            }
+
+        }
+
+        */
+    }
+
+    /*
+    public bool isGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, groundDistance, Jumpable);
+    }
+
+    public bool CanJump()
+    {
+        return isGrounded();
+    }
+    */
 
     [Command]
     void CmdSyncPlayerRotation()
