6c0a95316edd658337a4357f2b5fa28a75e55d96
HotshotCandy942 <HotshotCandy942@gmail.com>
2021-07-26 00:50:07 -0500

    deleted all of the old/broken scripts and reprogrammed looking

--- Assets/player scripts/old scipts/Player.cs
+++ Assets/player scripts/old scipts/Player.cs
@@ -8,17 +8,75 @@ public class Player : NetworkBehaviour
 
     public Rigidbody playerPhysBody;
     public Transform camTransformer;
+    public GameObject CameraSetup;
     public Camera worldCam;
     public Camera gunCam;
-    public lookHandler_fly looker;
-    public movement_fly mover;
+    //public lookHandler_fly looker;
+    //public movement_fly mover;
     public GameObject weaponHolder;
 
-    
+
+    public float sens = 100f;
+
+
+    public float yMouseInput = 0;
+    public float xMouseInput = 0;
+
+
+    private void Start()
+    {
+
+        
+
+        if (!isLocalPlayer)
+        {
+
+            
+
+            return;
+
+        }
+
+        Instantiate(CameraSetup, camTransformer);
+
+        
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+
+    }
+
     //input
+    private void Update()
+    {
+
+       
+
+        if (!isLocalPlayer) return;
+
+        float MouseX = Input.GetAxisRaw("Mouse X") * sens;
+        float MouseY = Input.GetAxisRaw("Mouse Y") * sens;
+
+
+        yMouseInput -= MouseY;
+        yMouseInput = Mathf.Clamp(yMouseInput, -90f, 90f);
+
+        xMouseInput -= MouseX;
+
+        camTransformer.transform.localRotation = Quaternion.Euler(Vector3.right * yMouseInput);
+        playerPhysBody.transform.rotation = Quaternion.Euler(Vector3.up * -xMouseInput);
+
+        
 
 
 
+
+    }
+
+    public void viewPunch(float r)
+    {
+        yMouseInput -= r;
+    }
 
 
 
