b598ae9bc750604578b3577423f4f6faea1e518c
HotshotCandy942 <HotshotCandy942@gmail.com>
2021-09-25 00:30:46 -0500

    Revert "more movement modifications"
    This reverts commit 2148790fec1999944104b47bb2678e6e9c79ac75.

--- Assets/scripts/PlayerScript.cs
+++ Assets/scripts/PlayerScript.cs
@@ -118,8 +118,6 @@ public class PlayerScript : NetworkBehaviour
     float y = 0;
     Vector3 groundNormal;
 
-    [SyncVar]
-    bool isCrouching;
 
     //backpack
     [SerializeField]
@@ -138,7 +136,7 @@ public class PlayerScript : NetworkBehaviour
 
         controls = new Inputmaster();
         controls.Player.jump.performed += _ => activateJump();
-        controls.Player.crouch.performed += _0 => crouch(); 
+        
         
     }
 
@@ -391,8 +389,7 @@ public class PlayerScript : NetworkBehaviour
 
     public bool isGrounded()
     {
-        
-        return Physics.Raycast(transform.position, -groundCheck.up, groundDistance, Jumpable); //Physics.CheckSphere(groundCheck.position, groundDistance, Jumpable);
+        return Physics.CheckSphere(groundCheck.position, groundDistance, Jumpable);
     }
 
     public bool CanJump()
@@ -407,11 +404,6 @@ public class PlayerScript : NetworkBehaviour
             jump = true;
         }
             
-    }
-
-    void crouch()
-    {
-
     }
 
     [Command]
