01eafec3d0286117943bbe66ec1d170aa230832c
HotshotCandy942 <HotshotCandy942@gmail.com>
2021-10-20 07:14:38 -0500

    can now choose transport in menu

--- Assets/scripts/newNetworkHUD.cs
+++ Assets/scripts/newNetworkHUD.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public class newNetworkHUD : NetworkManagerHUD
+
+public class newNetworkHUD : MonoBehaviour
 {
+    NetworkManager manager;
+
+    public bool showGUI = true;
+
+    public int offsetX;
+    public int offsetY;
 
     [SerializeField]
     List<Transport> transports;
@@ -12,11 +19,162 @@ public class newNetworkHUD : NetworkManagerHUD
     [SerializeField]
     private int index = 0;
 
-    
+    private Transport currentTransport;
+
+    void Awake()
+    {
+        manager = GetComponent<NetworkManager>();
+    }
 
     public Transport getCurrentTranport()
     {
-        return transports[index];
+        return currentTransport;
     }
 
+    void OnGUI()
+    {
+#pragma warning disable 618
+        if (!showGUI) return;
+#pragma warning restore 618
+
+        GUILayout.BeginArea(new Rect(10 + offsetX, 40 + offsetY, 215, 9999));
+        if (!NetworkClient.isConnected && !NetworkServer.active)
+        {
+            StartButtons();
+        }
+        else
+        {
+            StatusLabels();
+        }
+
+        // client ready
+        if (NetworkClient.isConnected && !NetworkClient.ready)
+        {
+            if (GUILayout.Button("Client Ready"))
+            {
+                NetworkClient.Ready();
+                if (NetworkClient.localPlayer == null)
+                {
+                    NetworkClient.AddPlayer();
+                }
+            }
+        }
+
+        StopButtons();
+
+        GUILayout.EndArea();
+    }
+
+    void StartButtons()
+    {
+        if (!NetworkClient.active)
+        {
+            // Server + Client
+            if (Application.platform != RuntimePlatform.WebGLPlayer)
+            {
+                if (GUILayout.Button("Host (Server + Client)"))
+                {
+                    manager.StartHost();
+                }
+            }
+
+            // Client + IP
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Client"))
+            {
+                manager.StartClient();
+            }
+            manager.networkAddress = GUILayout.TextField(manager.networkAddress);
+            GUILayout.EndHorizontal();
+
+            // Server Only
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+            {
+                // cant be a server in webgl build
+                GUILayout.Box("(  WebGL cannot be server  )");
+            }
+            else
+            {
+                if (GUILayout.Button("Server Only")) manager.StartServer();
+            }
+
+            // transport
+            /*
+            GUILayout.BeginHorizontal();
+            GUILayout.Button("Transport Index:");
+            int.TryParse(GUILayout.TextField(index.ToString()), out index);
+            GUILayout.EndHorizontal();
+            */
+
+            GUILayout.BeginVertical();
+            foreach(Transport t in transports)
+            {
+                if(GUILayout.Button(t.ToString()))
+                {
+                    currentTransport = t;
+                }
+            }
+            GUILayout.EndVertical();
+
+        }
+        else
+        {
+            // Connecting
+            GUILayout.Label("Connecting to " + manager.networkAddress + "..");
+            if (GUILayout.Button("Cancel Connection Attempt"))
+            {
+                manager.StopClient();
+            }
+        }
+    }
+
+    void StatusLabels()
+    {
+        // host mode
+        // display separately because this always confused people:
+        //   Server: ...
+        //   Client: ...
+        if (NetworkServer.active && NetworkClient.active)
+        {
+            GUILayout.Label($"<b>Host</b>: running via {Transport.activeTransport}");
+        }
+        // server only
+        else if (NetworkServer.active)
+        {
+            GUILayout.Label($"<b>Server</b>: running via {Transport.activeTransport}");
+        }
+        // client only
+        else if (NetworkClient.isConnected)
+        {
+            GUILayout.Label($"<b>Client</b>: connected to {manager.networkAddress} via {Transport.activeTransport}");
+        }
+    }
+
+    void StopButtons()
+    {
+        // stop host if host mode
+        if (NetworkServer.active && NetworkClient.isConnected)
+        {
+            if (GUILayout.Button("Stop Host"))
+            {
+                manager.StopHost();
+            }
+        }
+        // stop client if client-only
+        else if (NetworkClient.isConnected)
+        {
+            if (GUILayout.Button("Stop Client"))
+            {
+                manager.StopClient();
+            }
+        }
+        // stop server if server-only
+        else if (NetworkServer.active)
+        {
+            if (GUILayout.Button("Stop Server"))
+            {
+                manager.StopServer();
+            }
+        }
+    }
 }
