174bf02cc5ef84acc6dd16477a99b6f0731e7816
HotshotCandy942 <HotshotCandy942@gmail.com>
2022-01-05 07:19:23 -0600

    imprting raptor

---
+++ Assets/weapons/raptor/raptorScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raptorScript : gunClass
+{
+
+    [SerializeField]
+    public AudioClip ChamberClicksound;
+
+}
