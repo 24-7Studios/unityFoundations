e6a667ea7bf4d760a8e521a9e3bd631d7a7bbef4
HotshotCandy942 <HotshotCandy942@gmail.com>
2022-01-15 02:24:04 -0600

    got head pointing for playermodel (IK system)

--- Assets/player/playermodels/dude/DudePlayermodel.cs
+++ Assets/player/playermodels/dude/DudePlayermodel.cs
@@ -10,6 +10,12 @@ public class DudePlayermodel : PlayerModelClass
     private Animator anim;
 
     [SerializeField]
+    private Transform actualAimTarget;
+
+    [SerializeField]
+    private Transform virtualAimTarget;
+
+    [SerializeField]
     private float animationDamping;
 
     // Start is called before the first frame update
@@ -18,10 +24,20 @@ public class DudePlayermodel : PlayerModelClass
         
     }
 
+    public override void setPlayer(PlayerScript p)
+    {
+        base.setPlayer(p);
+
+        virtualAimTarget.SetParent(player.getCamTransformer());
+        virtualAimTarget.localPosition = Vector3.forward * 5;
+    }
+
     // Update is called once per frame
     void Update()
     {
         anim.SetFloat("x", player.getBasicInputMovement().normalized.x, animationDamping, Time.fixedDeltaTime);
         anim.SetFloat("y", player.getBasicInputMovement().normalized.z, animationDamping, Time.fixedDeltaTime);
+
+        actualAimTarget.position = virtualAimTarget.position;
     }
 }
