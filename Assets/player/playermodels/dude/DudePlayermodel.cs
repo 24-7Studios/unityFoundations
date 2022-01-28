c0d615102929baeee543081e8feaac8bb6220844
HotshotCandy942 <HotshotCandy942@gmail.com>
2022-01-28 07:16:38 -0600

    improved ik system. got correct holding positions

--- Assets/player/playermodels/dude/DudePlayermodel.cs
+++ Assets/player/playermodels/dude/DudePlayermodel.cs
@@ -111,7 +111,7 @@ public class DudePlayermodel : PlayerModelClass
 
         if(w.rightHoldPos != null)
         {
-            w.getWorldModelOb().transform.position = RightHoldParent.position - w.rightHoldPos.position/4;
+            w.getWorldModelOb().transform.position = RightHoldParent.position - (w.rightHoldPos.position - w.getWorldModelOb().transform.position) ;
         }
 
     }
