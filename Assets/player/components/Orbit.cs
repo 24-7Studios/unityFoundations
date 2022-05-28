f7d95ac451b48f283cf2dee7b36dbc89e5596f77
HotshotCandy942 <HotshotCandy942@gmail.com>
2022-05-27 23:50:20 -0500

    new death cam script

---
+++ Assets/player/components/Orbit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+
+    private Transform target;
+
+    private float active;
+
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
