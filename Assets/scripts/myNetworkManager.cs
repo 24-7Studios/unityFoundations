fad2c928446d0b2b561649bacb0d3ebe4103230e
HotshotCandy942 <HotshotCandy942@gmail.com>
2022-12-08 21:54:00 -0600

    L

--- Assets/scripts/myNetworkManager.cs
+++ Assets/scripts/myNetworkManager.cs
@@ -9,25 +9,6 @@ public class myNetworkManager : NetworkManager
     private newNetworkHUD hud;
 
 
-    public override void OnValidate()
-    {
-        // always >= 0
-        maxConnections = Mathf.Max(maxConnections, 0);
-
-        if (playerPrefab != null && playerPrefab.GetComponent<NetworkIdentity>() == null)
-        {
-            Debug.LogError("NetworkManager - Player Prefab must have a NetworkIdentity.");
-            playerPrefab = null;
-        }
-
-        // This avoids the mysterious "Replacing existing prefab with assetId ... Old prefab 'Player', New prefab 'Player'" warning.
-        if (playerPrefab != null && spawnPrefabs.Contains(playerPrefab))
-        {
-            Debug.LogWarning("NetworkManager - Player Prefab should not be added to Registered Spawnable Prefabs list...removed it.");
-            spawnPrefabs.Remove(playerPrefab);
-        }
-    }
-
     public override void Awake()
     {
         hud = GetComponent<newNetworkHUD>();
